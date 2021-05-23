b2d7d8d4c8ee91795b7b7b17102f18213eae1fb2
AAKonkin <konich1999@yandex.ru>
2021-05-23 19:03:49 +0300

    Problems with load project:(

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use('/api', routes)
 app.use(express.urlencoded);
+
 mongoose.connect(config.get('DB_URL'), {
     useUnifiedTopology: true,
     useNewUrlParser: true,
@@ -15,4 +16,5 @@ mongoose.connect(config.get('DB_URL'), {
     useFindAndModify: false
 });
 mongoose.connection.on('connected', () => console.log('ALREADY_CONNECTED_TO_DB'));
+
 app.listen(PORT, console.log(`ToDo SERVER START WORKING AT PORT: ${PORT}`));
